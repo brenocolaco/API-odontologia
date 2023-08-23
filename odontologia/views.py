c157633cb560a729648a162fe2c0001df5e7fbda
Breno Colaço <54214580+brenocolaco@users.noreply.github.com>
2023-08-23 17:28:00 -0300

    Views e Rotas Url

--- odontologia/views.py
+++ odontologia/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from odontologia.models import ACD, Aluno
+from odontologia.serializer import ACDSerializer, AlunoSerializer
 
 # Create your views here.
+
+class ACDsViewSet(viewsets.ModelViewSet):
+    '''Exibindo todos os ACDs'''
+    queryset = ACD.objects.all()
+    serializer_class = ACDSerializer
+
+class AlunosViewSet(viewsets.ModelViewSet):
+    '''Exibindo todos os alunos'''
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
--- setup/urls.py
+++ setup/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from odontologia.views import ACDsViewSet, AlunosViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('acds', ACDsViewSet, basename='ACDs')
+router.register('alunos', AlunosViewSet, basename='Alunos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
